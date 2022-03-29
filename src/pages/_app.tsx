e211c152b1c1c957d99d23e4d1383985bc1a5bd3
gus199 <augustine2903a@gmail.com>
2022-03-29 17:09:40 -0400

    Adding Navbar component and connect it to login/signUp pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import type { AppProps } from 'next/app';
 import '@/styles/global.css';
 import { UserContext } from '@/lib/context';
 import { useUserData } from '@/lib/hooks';
+import Layout from '@/layout/Navbar';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const { userData } = useUserData();
   return (
     <UserContext.Provider value={{ user: userData }}>
+      <Layout />
       <Component {...pageProps} />
     </UserContext.Provider>
   );
